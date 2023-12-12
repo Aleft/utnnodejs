f3d6a94fe161b4e7e6ac5b0498ffec70dbfca60c
Alexandros Diaz <alebmxft@gmail.com>
2023-12-12 16:52:28 -0300

    pocos cambios example

--- Clase-04/ExampleC4/routes/productos.js
+++ Clase-04/ExampleC4/routes/productos.js
@@ -5,27 +5,35 @@ const express = require("express");
 
 const routerProductos = express.Router();
 
-routerProductos.get("/productos", (req, res) => {
-    const categoria = req.query.categoria;
-    // Aquí puedes usar el parámetro de categoría para realizar una búsqueda de productos en esa categoría
-    res.send(`Realizar búsqueda de productos en la categoría "${categoria}"`);
-  });
-  
-  routerProductos.post("/productos", (req, res) => {
-    const producto = req.body;
-    // Aquí puedes guardar el nuevo producto en la base de datos o realizar otras operaciones relacionadas con el producto
-    res.send(`Guardar nuevo producto: ${JSON.stringify(producto)}`);
-  });
-  
-  routerProductos.post("/productos", (req, res) => {
-    // Lógica para crear un nuevo usuario
-    res.send("Producto creado correctamente");
-  });
-  
-  routerProductos.get('/:id', (req, res) => {
-    const productoId = req.params.id;
-    // Lógica para obtener información del usuario con el ID especificado
-    res.send(`Información del producto con ID ${JSON.stringify(productoId)}`);
-   });
+routerProductos.get("/", (req, res) => {
+  const productos = [
+    { id: 1, nombre: "Tablet" },
+    { id: 2, nombre: "PC" },
+  ];
+  res.json(productos);
+});
 
-  module.exports = routerProductos;
+// routerProductos.get("/productos", (req, res) => {
+//   const categoria = req.query.categoria;
+//   // Aquí puedes usar el parámetro de categoría para realizar una búsqueda de productos en esa categoría
+//   res.send(`Realizar búsqueda de productos en la categoría "${categoria}"`);
+// });
+
+routerProductos.post("/productos", (req, res) => {
+  const producto = req.body;
+  // Aquí puedes guardar el nuevo producto en la base de datos o realizar otras operaciones relacionadas con el producto
+  res.send(`Guardar nuevo producto: ${JSON.stringify(producto)}`);
+});
+
+routerProductos.post("/productos", (req, res) => {
+  // Lógica para crear un nuevo usuario
+  res.send("Producto creado correctamente");
+});
+
+routerProductos.get("/:id", (req, res) => {
+  const productoId = req.params.id;
+  // Lógica para obtener información del usuario con el ID especificado
+  res.send(`Información del producto con ID ${JSON.stringify(productoId)}`);
+});
+
+module.exports = routerProductos;
