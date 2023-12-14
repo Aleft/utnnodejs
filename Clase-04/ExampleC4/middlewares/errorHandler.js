44bbd5cec2400393f9ca4ad462da2e8c3e926ae2
Aleft <alebmxft@gmail.com>
2023-12-13 21:15:26 -0300

    actividad 4 finalizada

--- Clase-04/ExampleC4/middlewares/errorHandler.js
+++ Clase-04/ExampleC4/middlewares/errorHandler.js
@@ -1,8 +1,9 @@
-
-//Middleware para manejar errores 
+//Middleware para manejar errores
 const errorHandler = (err, req, res, next) => {
-    console.error(err);
-    res.status(err.status || 500).json({ error: err.message || "error en el servidor"});
+  console.error(err);
+  res
+    .status(err.status || 500)
+    .json({ error: err.message || "error en el servidor" });
 };
 
 module.exports = errorHandler;
